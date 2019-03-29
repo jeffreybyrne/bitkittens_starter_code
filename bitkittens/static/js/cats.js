d8a96298b354b6667246a6221f4045499947a8c6
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-29 14:43:46 -0400

    finished the assignment, and part of one of the stretch goals

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const catButton = document.querySelector('.summon-cats');
+  const catList = document.querySelector('#cat-list');
+  const catBook = document.querySelector('#cat-book-section');
+  catButton.addEventListener('click', () => {
+    console.log("hi")
+    axios.get("http://bitkittens.herokuapp.com/cats.json")
+      .then((resp) => {
+        if (catBook.style.display == '') {
+          catBook.style.display = 'inline-block';
+        }
+        cats = resp.data['cats'];
+        console.log(cats)
+        cats.forEach((cat, index) => {
+          catPic = document.createElement('img');
+          catPic.src = resp.data['cats'][index]['photo']
+          catPic.alt = `Photo of ${cat['name']}`
+          catBox = document.querySelector('#cat' + (index+1));
+          catBox.innerText = '';
+          catBox.appendChild(catPic);
+          catLi = document.createElement('li');
+          catLi.innerText = cat.name;
+          catList.appendChild(catLi);
+          console.log(catBox)
+          console.log(catPic)
+        })
+      })
+  })
 });
